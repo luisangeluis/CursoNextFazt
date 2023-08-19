66958070b097fc298e3e9312487ede69fd799fcc
luisangeluis <luisangeluis@gmail.com>
2023-08-19 17:42:04 -0600

    Started fazt course

---
+++ app/about/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutLayout = ({children}) => {
+  return (
+    <>
+      <nav>Layout de about</nav> 
+      {children}
+    </>
+  )
+}
+
+export default AboutLayout
