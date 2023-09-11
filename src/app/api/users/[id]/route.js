7f9fbfd93acaf32a555eeeacc3b64d22a2c68d27
luisangeluis <luisangeluis@gmail.com>
2023-09-11 14:55:08 -0600

    Environment section finished

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server";
 
 export const GET = async (request, { params }) => {
-  console.log(params);
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${params.id}`
-  );
+  console.log({ params });
+  const { searchParams } = new URL(request.url);
+  // console.log(searchParams);
+  console.log(searchParams.get("nombre"));
+
+  const response = await fetch(`${process.env.PLACEHOLDER_API}/users/${params.id}`);
   const data = await response.json();
   return NextResponse.json(data);
 };
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,10 +1,18 @@
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const response = await fetch(`${process.env.PLACEHOLDER_API}/users`);
   const data = await response.json();
   return NextResponse.json({ data });
 };
-export const POST = () => NextResponse.json({ message: "POST method" });
+
+export const POST = async(request) => {
+  const data = await request.json();
+
+  console.log({data});
+
+  return NextResponse.json({ message: "POST method" });
+}
+
 export const PUT = () => NextResponse.json({ message: "PUT method" });
 export const DELETE = () => NextResponse.json({ message: "DELETE method" });
